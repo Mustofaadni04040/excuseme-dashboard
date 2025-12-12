2ed2056b169b6d3059cc9223662adafdc89e9ad2
mustofa adny <adniadni04@gmail.com>
2025-12-12 16:48:09 +0700

    ui: responsive in StatsCard section

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -15,7 +15,7 @@ export default function StatsCard() {
   }, []);
 
   return (
-    <div className="max-w-[1100px] mx-auto grid grid-cols-3 mt-12 gap-8">
+    <div className="max-w-[1100px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 gap-8">
       <CardComponent>
         <div className="-mx-10 border-b-3 border-[#DBE8F4] px-10">
           <div className="flex items-center justify-between">
@@ -55,8 +55,8 @@ export default function StatsCard() {
 
           <span className="text-4xl font-bold block my-2">85</span>
         </div>
-        <div className="flex items-center gap-8 justify-between">
-          <div className="flex flex-col gap-1 items-end">
+        <div className="flex-col lg:flex-row flex mt-3 lg:mt-0 items-center gap-2 lg:gap-8 justify-between">
+          <div className="flex flex-col gap-1 items-center lg:items-end">
             <p className="flex items-center gap-2 text-sm tracking-wider">
               <span className="font-semibold">60%</span>
               Daily Goal
@@ -66,8 +66,8 @@ export default function StatsCard() {
             </p>
           </div>
 
-          <div className="relative">
-            <div className="absolute top-1/2 left-25 z-50 transform -translate-x-1/2 text-[10px] font-semibold bg-white shadow-[0px_0px_25px_0px_rgba(0,0,0,0.1)] px-2 py-1 rounded-sm">
+          <div className="relative flex items-center justify-center">
+            <div className="absolute top-1/2 left-28 lg:left-2/3 z-50 transform -translate-x-1/2 text-[10px] font-semibold bg-white shadow-[0px_0px_25px_0px_rgba(0,0,0,0.1)] px-2 py-1 rounded-sm">
               100%
             </div>
 
